8b4dc26e20f4b75d281a7d8c20a6a731d60aeb9b
kvazimode <kvazimode2@gmail.com>
2020-06-15 23:05:10 +0300

    main component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/main/main.jsx';
+
+ReactDOM.render(
+    <Main />,
+    document.querySelector(`#root`)
+);
