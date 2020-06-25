bc0c24462fda12e2c1637b4d1447f52d36db37ba
kvazi <kvazimode2@gmail.com>
2020-06-25 15:34:37 +0300

    offer card separation from main

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,8 +3,8 @@ import Main from '../main/main.jsx';
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {count} = props;
-  return <Main count={count}/>;
+  const {count, offerTitles} = props;
+  return <Main count={count} offerTitles={offerTitles}/>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
-  const setup = {count: 666};
+  const setup = {
+    count: 666,
+    offerTitles: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`]
+  };
   ReactDOM.render(
-      <App count={setup.count}/>,
+      <App count={setup.count} offerTitles={setup.offerTitles}/>,
       document.querySelector(`#root`)
   );
 };
