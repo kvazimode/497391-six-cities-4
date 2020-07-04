5b9ea20ccc3f512e1ea290d8e52c75ed6e272d97
kvazimode <kvazimode2@gmail.com>
2020-07-05 00:02:46 +0300

    mocks for cards

---
+++ src/mocks/offers.js
@@ -0,0 +1,48 @@
+const offers = [
+  {
+    id: 1,
+    title: `Beautiful & luxurious apartment at graet location`,
+    imgLink: `img/apartment-01.jpg`,
+    value: 120,
+    type: `Apartment`,
+    isPremium: true,
+    rating: 80,
+    coord: [52.3909553943508, 4.85309666406198],
+    city: `Amsterdam`
+  },
+  {
+    id: 2,
+    title: `Wood and stone place`,
+    imgLink: `img/room.jpg`,
+    value: 80,
+    type: `Private room`,
+    isPremium: false,
+    rating: 80,
+    coord: [53.556, 9.963],
+    city: `Hamburg`
+  },
+  {
+    id: 3,
+    title: `You can't live closer to Eiffel Tower`,
+    imgLink: `img/apartment-02.jpg`,
+    value: 132,
+    type: `Apartment`,
+    isPremium: false,
+    rating: 80,
+    coord: [48.856626, 2.292919],
+    city: `Paris`
+  },
+  {
+    id: 4,
+    title: `Nice, cozy, warm big bed apartment`,
+    imgLink: `img/apartment-03.jpg`,
+    value: 180,
+    type: `Apartment`,
+    isPremium: true,
+    rating: 100,
+    coord: [50.99, 6.93],
+    city: `Cologne`
+  }
+];
+
+export default offers;
