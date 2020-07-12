655d78db5b90c29f63665bbfe67436be7ef356ce
kvazimode <kvazimode2@gmail.com>
2020-07-12 14:16:19 +0300

    app component became class

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import Offer from '../offer/offer.jsx';
 import Main from '../main/main.jsx';
 
-const handleTitleClick = () => {};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-const App = (props) => {
-  const {count, offerList} = props;
-  return <Main count={count} offerList={offerList} onTitleClick={handleTitleClick}/>;
-};
+  _handleTitleClick() {}
+  _renderApp() {
+    const {count, offerList} = this.props;
+    return <Main count={count} offerList={offerList} onTitleClick={this._handleTitleClick}/>;
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route excat path="/desc">
+            <Offer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   count: PropTypes.number.isRequired,
